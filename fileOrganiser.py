afc818fd77c114343816e868e8308b204d076455
FlashLeap <84667872+FlashLeap@users.noreply.github.com>
2021-12-29 14:57:14 +0530

    renamed by mistake

---
+++ fileOrganiser.py
@@ -0,0 +1,10 @@
+import os, time, shutil
+    
+path = r"D:\Mrunank Bathe\emoji\jpg"
+now = time.time()
+
+for f in os.listdir(path):
+    f = os.path.join(path, f)
+    if os.stat(f).st_mtime < now - 7 * 86400:
+        if os.path.isfile(f):
+            os.remove(os.path.join(path, f))
